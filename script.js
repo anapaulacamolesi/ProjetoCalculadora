a3d6d148455072b6224dbccc05ba97219cb3ebf4
Ana <anapaulacamolesi@gmail.com>
2021-11-10 20:48:16 -0300

    inclusão do JS

---
+++ script.js
@@ -0,0 +1,64 @@
+//tudo errado - tem que adicionar o número
+
+// function numero() {
+//   var numEscolhido - Number(document.getElementById('0').value)
+// }
+
+function adicionar(num) {
+  var numAtual = document.form.txt.value;
+  var numNovo = num;
+  document.form.txt.value = numAtual + numNovo;
+}
+
+
+function botaoZero() {
+  var numEscolhido = Number(document.getElementById('0').value)
+  num.innerHTML = numEscolhido
+}
+function botaoUm() {
+  var numEscolhido = Number(document.getElementById('1').value)
+  num.innerHTML = numEscolhido
+}
+
+// const um = 1 
+// const dois = 2
+// const tres = 3 
+// const quatro = 4
+// const cinco = 5 
+// const seis = 6
+// const sete = 7
+// const oito = 8
+// const nove = 9
+
+
+  // var num1 = Number(document.getElementById('1').value).addEventListener('click', function(){
+  //     num.innerHTML = num1
+  // });
+// var num = Number(document.getElementById.value)
+// num.innerHTML = 
+
+// var add = document.querySelector('#num')
+// add.innerHTML = num1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
